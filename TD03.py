86b27a66f30581b2b0e26eee04e21cece143b051
Océane MACHADO <oceane.machado@ens.uvsq.fr>
2021-03-23 06:05:39 +0100

    .

--- TD03.py
+++ TD03.py
@@ -1,32 +1,33 @@
-var ballr = 10;
-var rowcolors = ["#FF1C0A", "#FFFD0A", "#00A308", "#0008DB", "#EB0093"];
-var paddlecolor = "#FFFFFF";
-var ballcolor = "#FFFFFF";
-var backcolor = "#000000";
+ballr = 10
+rowcolors = ["#FF1C0A", "#FFFD0A", "#00A308", "#0008DB", "#EB0093"]
+paddlecolor = "#FFFFFF"
+ballcolor = "#FFFFFF"
+backcolor = "#000000"
 
-function draw() {
-  ctx.fillStyle = backcolor;
-  clear();
-  ctx.fillStyle = ballcolor;
-  circle(x, y, ballr);
+def draw() :
+    { ctx.fillStyle = backcolor;
+    clear();
+    ctx.fillStyle = ballcolor;
+    circle(x, y, ballr);
 
-  if (rightDown) paddlex += 5;
-  else if (leftDown) paddlex -= 5;
-  ctx.fillStyle = paddlecolor;
-  rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
+    if (rightDown) paddlex += 5;
+    else if (leftDown) paddlex -= 5;
+    ctx.fillStyle = paddlecolor;
+    rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
 
-  drawbricks();
+    drawbricks();
 
-  // La balle est-elle rentrée en collision avec une brique ?
-  rowheight = BRICKHEIGHT + PADDING;
-  colwidth = BRICKWIDTH + PADDING;
-  row = Math.floor(y/rowheight);
-  col = Math.floor(x/colwidth);
-  // Si c'est le cas, faire rebondir la balle et marquer la brique comme démolie
-  if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
-    dy = -dy;
-    bricks[row][col] = 0;
-  }
+  ## La balle est-elle rentrée en collision avec une brique ?
+    rowheight = BRICKHEIGHT + PADDING;
+    colwidth = BRICKWIDTH + PADDING;
+    row = Math.floor(y/rowheight);
+    col = Math.floor(x/colwidth);
+  ## Si c'est le cas, faire rebondir la balle et marquer la brique comme démolie
+    if (y < NROWS * rowheight and row >= 0 and col >= 0 and bricks[row][col] == 1) :
+        {
+        dy = -dy;
+        bricks[row][col] = 0;
+        }
   
   // On prend en compte la paroi de la balle et non son centre
   if (x + dx + ballr > WIDTH || x + dx - ballr < 0)
@@ -35,7 +36,7 @@ function draw() {
   if (y + dy - ballr < 0)
     dy = -dy;
   else if (y + dy + ballr > HEIGHT - paddleh) {
-    if (x > paddlex && x < paddlex + paddlew) {
+    if (x > paddlex and x < paddlex + paddlew) {
       // On renvoie la balle différemment selon son lieu d'atterrissage
       dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
       dy = -dy;
@@ -48,5 +49,5 @@ function draw() {
   y += dy;
 }
 
-init();
-initbricks();
+init(
+initbricks()
