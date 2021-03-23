d8fc44bc829b62f4408512929267f682b9addb61
Océane MACHADO <oceane.machado@ens.uvsq.fr>
2021-03-23 06:10:34 +0100

    .

--- TD03.py
+++ TD03.py
@@ -3,6 +3,41 @@ rowcolors = ["#FF1C0A", "#FFFD0A", "#00A308", "#0008DB", "#EB0093"]
 paddlecolor = "#FFFFFF"
 ballcolor = "#FFFFFF"
 backcolor = "#000000"
+x = 150
+y = 150
+dx = 2;  dy = 4
+var WIDTH
+var HEIGHT
+var ctx
+
+def init() :
+    {
+    ctx = $('#canvas')[0].getContext("2d");
+    WIDTH = $("#canvas").width();
+    HEIGHT = $("#canvas").height();
+    return setInterval(draw, 10);
+    }
+
+def circle(x,y,r) :
+    {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+    }
+
+def rect(x,y,w,h) :
+    {
+    ctx.beginPath();
+    ctx.rect(x,y,w,h);
+    ctx.closePath();
+    ctx.fill();
+    }
+
+def clear() :
+    {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    
 
 def draw() :
     { ctx.fillStyle = backcolor;
