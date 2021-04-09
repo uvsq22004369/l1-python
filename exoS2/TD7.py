f95d9f1580774b70cad3725c295c5916a348355a
Océane MACHADO <oceane.machado@ens.uvsq.fr>
2021-04-09 12:07:03 +0200

    .

--- exoS2/TD7.py
+++ exoS2/TD7.py
@@ -1,26 +1,24 @@
 import tkinter as tk
 
 def decalage(lettre_message,lettre_cle):
-    res = chr(ord(lettre_message) + ord(lettre_cle)) and ((1 << 8) -1)
-    return res
+    return chr(ord(lettre_message)+ord(lettre_cle) & ((1<<8)-1))
 
 def dec_texte(texte,cle):
-    res = ""
-    i, j = 0, 0
-    while i < len(texte) :
-        res += decalage(texte[i], cle[j])
-        i += 1
-        j += 1
-        if j == len(cle) : 
-            j = 0
-    return res
+    texte_code = ""
+    t, c = 0, 0
+    while len(texte_code) < len(texte):
+      texte_code += decalage(texte[t], cle[c])
+      t, c = t + 1, c + 1
+      if c == len(cle):
+        c = 0
+    return texte_code
+
 
 def chiffre():
-    resultat.delete(0,tk.END)
-    if (entree_texte.get() == "" + entree_cle.get() == "") : 
-        resultat.insert(0),"Il manque des argument"
-    else :
-       resultat.insert(0,dec_texte(entree_texte.get(),entree_cle.get()))
+    resultat.delete(0, tk.END)
+    if entree_texte.get() == "" or entree_cle.get() == "":
+      resultat.insert(0, "Il manque quelque chose en entrée :/")
+    resultat.insert(0, dec_texte(entree_texte.get(), entree_cle.get()))
 
 # On peut aussi faire 2 fonction différente
 #
@@ -32,9 +30,21 @@ def chiffre():
 #    if (entree_texte.get() == "" + entree_cle.get() == "") : 
 #        resultat.insert(0),"Il manque des argument"
 
+def dechiffrement(texte_a_decoder, cle):
+    texte_decode = ""
+    t, c = 0, 0
+    while len(texte_decode) < len(texte_a_decoder):
+      texte_decode += decalage(texte_a_decoder[t], chr(256-ord(cle[c])))
+      t, c = t + 1, c + 1
+      if c == len(cle):
+        c = 0
+    return texte_decode
 
 def dechiffre():
-    pass
+    if entree_texte.get() == "" or entree_cle.get() == "":
+      resultat.insert(0, "Il manque quelque chose en entrée :/")
+    else:
+      label_res.config(text = dechiffrement(resultat.get(), entree_cle.get()))
 
 def chiffre_xor(lettre_message,lettre_cle):
     pass
@@ -63,5 +73,7 @@ bouton_decoder.grid(row=2, column=1)
 resultat=tk.Entry(racine,width = 50, font = ("helvetica", "20"))
 resultat.grid(row=3,column=0)
 
+label_res=tk.Label(racine,font = ("helvetica", "20"), text="Déchiffement ici.")
+label_res.grid(row = 3, column=1)
 
 racine.mainloop()
