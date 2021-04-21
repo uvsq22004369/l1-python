940c5b67107a9dc972cbeef3ab8443a64bc1f7d0
Océane MACHADO <oceane.machado@ens.uvsq.fr>
2021-04-21 21:32:07 +0200

    .

--- exoS2/Projet2_2.py
+++ exoS2/Projet2_2.py
@@ -1,18 +1,93 @@
-import tkinter as tk
-import numpy as np
-import PIL as pil
+##################################################
+# Projet Robot Ricochet
+# Groupe BI: 1
+# Mathieu LAM
+# Océane MACHADO
+# Aurélie ALVET
+# Timothé PEYREIGNE
+# Thushanth JEYAKANTHN
+# Yanis MERBAH
+# https://github.com/uvsq21915611/projet_robot
+###################################################
 
+import tkinter as tk
+racine = tk.Tk()
+racine.title("Projet robot ricochet")
+racine.configure(width = 300, height = 300, bg ='beige')
+
+c = 50                         # Longueur d'un côté d'une case
+n = 16                         # Nombre de cases par ligne et par colonne
+cases = []
+WIDTH = n*c+2
+HEIGHT = WIDTH
 colors = ["rouge",  "jaune", "vert", "bleu"]
 
-def fermer_fenetre():
-    racine.destroy()
 
 
+
+canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg="white")
+canvas.grid()
+
+
+for ligne in range(n):
+    transit = []
+    for colonne in range(n):
+        transit.append(canvas.create_rectangle(colonne*c+2, ligne*c+2, (colonne+1)*c+2, (ligne+1)*c+2))
+    cases.append(transit)
+
+
+
+mur_vertical1 = canvas.create_rectangle(250-2, 0, 250+5, 50+2, fill="black")
+mur_vertical2 = canvas.create_rectangle(600-2, 0, 600+5, 50+2, fill="black")
+mur_vertical3 = canvas.create_rectangle(350-2, 50+2, 350+5, 100+2, fill="black")
+mur_vertical4 = canvas.create_rectangle(50-2, 100+2, 50+5, 150+2, fill="black")
+mur_vertical5 = canvas.create_rectangle(600-2, 100+2, 600+5, 150+2, fill="black")
+mur_vertical6 = canvas.create_rectangle(700-2, 150+2, 700+5, 200+2, fill="black")
+mur_vertical7 = canvas.create_rectangle(500-2, 200+2, 500+5, 250+2, fill="black")
+mur_vertical8 = canvas.create_rectangle(350-2, 250+2, 350+5, 300+2, fill="black")
+mur_vertical9 = canvas.create_rectangle(600-2, 250+2, 600+5, 300+2, fill="black")
+mur_vertical10 = canvas.create_rectangle(100-2, 300+2, 100+5, 350+2, fill="black")
+mur_vertical11 = canvas.create_rectangle(650-2, 450+2, 650+5, 500+2, fill="black")
+mur_vertical12 = canvas.create_rectangle(200-2, 500+2, 200+5, 550+2, fill="black")
+mur_vertical13 = canvas.create_rectangle(300-2, 550+2, 300+5, 600+2, fill="black")
+mur_vertical14 = canvas.create_rectangle(100-2, 600+2, 100+5, 650+2, fill="black")
+mur_vertical15 = canvas.create_rectangle(450-2, 600+2, 450+5, 650+2, fill="black")
+mur_vertical16 = canvas.create_rectangle(200-2, 650+2, 200+5, 700+2, fill="black")
+mur_vertical17 = canvas.create_rectangle(700-2, 650+2, 700+5, 700+2, fill="black")
+mur_vertical18 = canvas.create_rectangle(600-2, 700+2, 600+5, 750+2, fill="black")
+mur_vertical19 = canvas.create_rectangle(200-2, 750+2, 200+5, 800, fill="black")
+mur_vertical20 = canvas.create_rectangle(700-2, 750+2, 700+5, 800, fill="black")
+
+mur_horizontal1 = canvas.create_rectangle(50+2, 100-2, 100+2, 100+5, fill="black")
+mur_horizontal2 = canvas.create_rectangle(300+2, 100-2, 350+2, 100+5, fill="black")
+mur_horizontal3 = canvas.create_rectangle(550+2, 150-2, 600+2, 150+5, fill="black")
+mur_horizontal4 = canvas.create_rectangle(650+2, 150-2, 700+2, 150+5, fill="black")
+mur_horizontal5 = canvas.create_rectangle(300+2, 250-2, 350+2, 250+5, fill="black")
+mur_horizontal6 = canvas.create_rectangle(500+2, 250-2, 550+2, 250+5, fill="black")
+mur_horizontal7 = canvas.create_rectangle(600+2, 250-2, 650+2, 250+5, fill="black")
+mur_horizontal8 = canvas.create_rectangle(0, 300-2, 50+2, 300+5, fill="black")
+mur_horizontal9 = canvas.create_rectangle(750+2, 300-2, 800+2, 300+5, fill="black")
+mur_horizontal10 = canvas.create_rectangle(100+2, 350-2, 150+2, 350+5, fill="black")
+mur_horizontal11 = canvas.create_rectangle(600+2, 450-2, 650+2, 450+5, fill="black")
+mur_horizontal12 = canvas.create_rectangle(0, 500-2, 50+2, 500+5, fill="black")
+mur_horizontal13 = canvas.create_rectangle(750+2, 500-2, 800+2, 500+5, fill="black")
+mur_horizontal14 = canvas.create_rectangle(150+2, 550-2, 200+2, 550+5, fill="black")
+mur_horizontal15 = canvas.create_rectangle(250+2, 550-2, 300+2, 550+5, fill="black")
+mur_horizontal16 = canvas.create_rectangle(450+2, 600-2, 500+2, 600+5, fill="black")
+mur_horizontal17 = canvas.create_rectangle(100+2, 650-2, 150+2, 650+5, fill="black")
+mur_horizontal18 = canvas.create_rectangle(200+2, 650-2, 250+2, 650+5, fill="black")
+mur_horizontal19 = canvas.create_rectangle(700+2, 700-2, 750+2, 700+5, fill="black")
+mur_horizontal20 = canvas.create_rectangle(550+2, 750-2, 600+2, 750+5, fill="black")
+
+carre_restart = canvas.create_rectangle(350+2, 350+2, 450+2, 450+2, fill="black")
+
+
+#Crée les robot et leurs paramètres
 def robot_couleur_rouge():
     
-    x, y = LARGEUR // 2, HAUTEUR // 2
+    x, y = WIDTH // 2, HEIGHT // 2
     dx, dy = 2, 4
-    rayon = 20
+    rayon = 10
     cercle = canvas.create_oval((x-rayon, y-rayon),
                                 (x+rayon, y+rayon),
                                 fill="rouge")
@@ -20,9 +95,9 @@ def robot_couleur_rouge():
 
 def robot_couleur_jaune():
     
-    x, y = LARGEUR // 2, HAUTEUR // 2
+    x, y = WIDTH // 2, HEIGHT // 2
     dx, dy = 2, 4
-    rayon = 20
+    rayon = 10
     cercle = canvas.create_oval((x-rayon, y-rayon),
                                 (x+rayon, y+rayon),
                                 fill="jaune")
@@ -30,9 +105,9 @@ def robot_couleur_jaune():
 
 def robot_couleur_vert():
     
-    x, y = LARGEUR // 2, HAUTEUR // 2
+    x, y = WIDTH // 2, HEIGHT // 2
     dx, dy = 2, 4
-    rayon = 20
+    rayon = 10
     cercle = canvas.create_oval((x-rayon, y-rayon),
                                 (x+rayon, y+rayon),
                                 fill="vert")
@@ -40,54 +115,53 @@ def robot_couleur_vert():
 
 def robot_couleur_bleu():
     
-    x, y = LARGEUR // 2, HAUTEUR // 2
+    x, y = WIDTH // 2, HEIGHT // 2
     dx, dy = 2, 4
-    rayon = 20
+    rayon = 10
     cercle = canvas.create_oval((x-rayon, y-rayon),
                                 (x+rayon, y+rayon),
                                 fill="bleu")
 
     return [cercle, dx, dy]
 
+#Définit quel robot de couleur est actuellement sélectionné
+def selectionne():
+    pass
 
-def carre():
-    global objets
-    x = rd.randint(0, 100)
-    y = rd.randint(0, 100)
-    objets.append(canvas.create_rectangle((x, y), (x+100, y+100), fill=""))
+#Associe les touches directionnelles du clavier "haut","bas","gauche","droite" au déplacement du robot
+def bind():
+    pass
 
+#Permet de faire arrêter le robot s'il rencontre un obstacle / distance maximale de déplacement autorisé
+def bounce():
+    pass
 
+#Affiche un rectangle de couleur permettant de montrer les directions possibles par le robot 
+def show_Path():
+    pass
 
+#Fait recommencer le jeu
+def reset():
+    pass
 
+#Affiche le score / liste des meilleurs scores
+def score():
+    pass
 
+#Affiche le nombre de mouvements effectués
+def compteur():
+    pass
 
+#Permet d'enregistrer l'état de la partie (position des robots/nombres de mouvements/ ect ... )
+def save():
+    pass
 
+#Permet de charger les éléments précédemment sauvegardés
+def load():
+    pass
 
+#Permet d'éditer le plateau en rajoutant des éléments 
+def editor():
+    pass
 
-
-
-
-
-
-root = tk.Tk()
-root.title("Projet Robot Ricochet")
-
-root.geometry("600x600")
-container = tk.Frame(root, bg = "white")
-container.pack(expand = True, fill = "both")
-drawArea = tk.Canvas(container, bg = "gray")
-drawArea.pack()
-
-
-bouton = tk.Button(racine, text="Quitter", fg="red",command=fermer_fenetre)
-bouton.grid(column=0, row=1)
-
-bouton_undo = tk.Button(racine, text="Undo", fg="red", command=undo)
-bouton_undo.grid(row=0, column=2)
-
-label = tk.Label(racine, text=str(cpt))
-label.grid(column=0, row=3)
-
-canvas.bind("<Button-1>", compteur)
-
-root.mainloop()
+racine.mainloop()
